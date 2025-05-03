06962d7971828a846e4ab376d0fa162a39bbced6
UrjaChakraborty <uchakraborty@hawk.iit.edu>
2025-05-02 19:10:33 -0500

    readme moved

--- CS430Project.py
+++ CS430Project.py
@@ -1,3 +1,16 @@
+#Team members: Urja Chakraborty and Daisy Jiang
+
+#Programming Language Used: Python 
+
+#Brief Description: This program implements a dynamic programming solution for BTSHMULJWNI. Given a list of jobs 
+#(each with release time and deadline) and machine types (with capacity and cost), it computes an optimal batch-based schedule with minimum cost,
+#as per the definition and constraints in the project description and the referenced research paper. Please feel free to peruse 
+#the comments added, for insight into how the program works.
+
+#External Resources Used:
+#The provided research paper about Online Flexible Busy Time Scheduling on
+#Heterogeneous Machines.
+
 import sys
 import os
 
